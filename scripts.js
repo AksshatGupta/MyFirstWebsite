2305ccbd4bf9a571ccac2b2db6aeecdea5e402fb
AksshatGupta <aksshatyashgupta@gmail.com>
2018-10-12 22:19:44 -0400

    added javascript components

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
-console.log("Ready");
-alert("Hi");
+var z = "Running";
+function consoleZ(z) {
+  console.log(z);
+}
+consoleZ(z);
+
+var a = 10, b = 20;
+
+function multiply(x, y) {
+  console.log(x+ "*" +y);
+}
+
+multiply(a, b);
+var x = [10, 2, 30];
+console.log(x);
+
+document.getElementById("photo").addEventListener("click", sayHi);
+
+function sayHi() {
+  document.getElementById("helloBox").innerHTML = "Hello";
+  document.body.style.backgroundColor = rgb(255,130,255);
+}
